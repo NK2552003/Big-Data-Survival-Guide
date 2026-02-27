e56b82dbc367dc96b02d461b95dab87869008b11
NK2552003 <nk2552003@gmail.com>
2026-02-27 15:12:57 +0530

    Update layout metadata handling and clean up configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: isProd ? '/Big-Data-Survival-Guide' : '',
-  assetPrefix: isProd ? '/Big-Data-Survival-Guide/' : '',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   reactStrictMode: true,
   images: {
